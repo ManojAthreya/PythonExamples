142c703fbdf886e34b677eb93c1530428faea0bf
Manoj Athreya A <39020374+ManojAthreya@users.noreply.github.com>
2023-05-09 23:39:04 -0400

    loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+#FOR LOOPS
+print("FOR Loops")
+for x in range(1,5):
+    print(x)
+
+for y in range(1,10,2):
+    print(y)
+
+#WHILE LOOPS
+print("WHILE Loops")
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # This is the same as count = count + 1
